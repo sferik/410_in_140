73ec3fde720ece82b0f9ddf273e92b8d88d8f6c0
Erik Michaels-Ober <sferik@gmail.com>
2013-03-06 09:49:51 -0800

    Use tabs instead of spaces

--- server.rb
+++ server.rb
@@ -1,9 +1,9 @@
 require 'socket';
 s = TCPServer.new(80);
 loop {
- Thread.start(s.accept) { |c|
-  c.gets "\n\r";
-  c.puts "HTTP/1.1 410 Gone";
-  c.close;
- }
+	Thread.start(s.accept) { |c|
+		c.gets "\n\r";
+		c.puts "HTTP/1.1 410 Gone";
+		c.close;
+	}
 }
