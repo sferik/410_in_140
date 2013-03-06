8929f28488d2a35ec271d7cd78dd3fd3a3080417
Erik Michaels-Ober <sferik@gmail.com>
2013-03-06 09:59:10 -0800

    Use Ruby

--- spec.rb
+++ spec.rb
@@ -1,8 +1,9 @@
 require 'rspec'
 
 describe "the server" do
-  it "should be less than or equal to 140 characters" do
-    length = `wc -m server.rb`.split[0].to_i
-    expect(length).to be <= 140
-  end
+	it "should be less than or equal to 140 characters" do
+		file_size = File.size("server.rb")
+		puts file_size
+		expect(file_size).to be <= 140
+	end
 end
